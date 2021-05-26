f859d2f64fd8659f51d25f170e0369fcc27805ca
Runroom <software@runroom.com>
2021-05-26 15:18:05 +0000

    Fix split workflow

---
+++ src/Factory/TranslationFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Runroom package.
+ *
+ * (c) Runroom <runroom@runroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Runroom\TranslationBundle\Factory;
+
+use Runroom\TranslationBundle\Entity\Translation;
+use Zenstruck\Foundry\ModelFactory;
+
+/** @extends ModelFactory<Translation> */
+final class TranslationFactory extends ModelFactory
+{
+    /**
+     * @param string[] $locales
+     * @param array<string, mixed> $defaultAttributes
+     */
+    public function withTranslations(array $locales, array $defaultAttributes = []): self
+    {
+        return $this->addState([
+            'translations' => TranslationTranslationFactory::createMany(\count($locales), array_merge($defaultAttributes, [
+                'locale' => self::faker()->unique()->randomElement($locales),
+            ])),
+        ]);
+    }
+
+    /** @return array<string, mixed> */
+    protected function getDefaults(): array
+    {
+        return [
+            'key' => self::faker()->unique()->word(),
+        ];
+    }
+
+    protected static function getClass(): string
+    {
+        return Translation::class;
+    }
+}
